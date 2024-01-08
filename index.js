89d8bac676b3d5c0759760e13d8f4dbcc760ce3a
Gil0823 <jeong00400@kangwon.ac.kr>
2024-01-08 11:25:10 +0900

    기본 라우터(링크처리?) 생성
    - 최초 서버주소에 접속시 백엔드에서 처리해줄 함수 생성
    - 서버주소에 hello 까지 붙힌 링크에 접속시 백엔드에서 처리해줄 함수 생성

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const app = express();
 app.listen(3000, function(req, res) {
     console.log("서버 실행중...");
 });
+
+// nodejs 서버주소(https://nodejs-con-dcoxe.run.goorm.site)에 최초 접속시 호출되는 함수
+app.get('/', function(req, res){
+    res.send("Hello World!");
+});
+
+// https://nodejs-con-dcoxe.run.goorm.site/hello에 접속시 호출되는 함수
+app.get('/hello', function(req, res){
+    res.send("Hello Hello Hello~~~");
+});
